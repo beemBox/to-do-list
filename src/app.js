0e91a65668e36886dc40d512b401a7546976907a
Mariano Silva <silva.mariano.s@gmail.com>
2022-02-06 15:21:31 -0300

    Adding observable and databinding, changing modules names

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import { registerNav } from './nav.js'
+
+const app = async () => {
+  alert('modulo abierto')
+  registerNav()
+}
+
+
+document.addEventListener('DOMContentLoaded', app);
---
+++ src/nav.js
@@ -0,0 +1,6 @@
+export class Nav extends HTMLElement {
+  super()
+  this.
+}
+
+export const registerNav = () => customElements.define('nav-bar', Nav)
